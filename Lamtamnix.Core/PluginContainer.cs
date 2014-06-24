983798a58605b4c9d22aa0171d1e1585c6fa1ac4
Alexander Elyseev <alexander.elyseev@gmail.com>
2014-06-24 23:45:54 +0600

    Implemented gathering information about usage of system resource by plugins.

--- Lamtamnix.Core/PluginContainer.cs
+++ Lamtamnix.Core/PluginContainer.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Diagnostics.Contracts;
+    using System.Threading.Tasks;
 
     /// <summary>
     /// Class container with loaded plugins.
@@ -24,6 +25,7 @@
         /// <summary>
         /// Plugins stored in this container.
         /// </summary>
+        /// <exception cref="ObjectDisposedException">You can not use this instance after disposing.</exception>
         public IEnumerable<TPlugin> Plugins
         {
             get
@@ -54,6 +56,46 @@
         ~PluginContainer()
         {
             Dispose(false);
+        }
+
+        /// <summary>
+        /// Returns information about usage of system resources by the plugins in this container.
+        /// </summary>
+        /// <exception cref="ObjectDisposedException">You can not use this instance after disposing.</exception>
+        /// <exception cref="InvalidOperationException">You have to enable <see cref="AppDomain.MonitoringIsEnabled"/> before using.</exception>
+        /// <returns>Usage of system resources by plugins.</returns>
+        public Dictionary<TPlugin, ResourceUsageInfo> GetResourcesUsage()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(null);
+
+            if (!AppDomain.MonitoringIsEnabled)
+                throw new InvalidOperationException("You have to enable appdomain monitoring (AppDomain.MonitoringIsEnabled).");
+
+            Dictionary<TPlugin, ResourceUsageInfo> data = new Dictionary<TPlugin, ResourceUsageInfo>(_plugins.Count);
+            foreach (var entry in _plugins)
+            {
+                AppDomain pluginDomain = entry.Value;
+                var usageInfo = new ResourceUsageInfo
+                {
+                    SurvivedMemorySize = pluginDomain.MonitoringSurvivedMemorySize,
+                    TotalAllocatedMemorySize = pluginDomain.MonitoringTotalAllocatedMemorySize,
+                    TotalProcessorTime = pluginDomain.MonitoringTotalProcessorTime
+                };
+                data[entry.Key] = usageInfo;
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Returns information about usage of system resources by the plugins in this container.
+        /// Asynchronous version of <see cref="GetResourcesUsage"/>.
+        /// </summary>
+        /// <returns>Usage of system resources by plugins.</returns>
+        public async Task<Dictionary<TPlugin, ResourceUsageInfo>> GetResourcesUsageAsync()
+        {
+            return await Task.Factory.StartNew(() => GetResourcesUsage());
         }
 
         /// <summary>
---
+++ Lamtamnix.Core/ResourceUsageInfo.cs
@@ -0,0 +1,29 @@
+﻿namespace Lamtamnix.Core
+{
+    using System;
+
+    /// <summary>
+    /// Class-container with information about usage of system resources by some plugin.
+    /// </summary>
+    public sealed class ResourceUsageInfo
+    {
+        /// <summary>
+        /// Gets the number of bytes that survived the last collection and that are known to be referenced
+        /// by the application domain with the plugin (<see cref="AppDomain.MonitoringSurvivedMemorySize"/>).
+        /// </summary>
+        public Int64 SurvivedMemorySize { get; set; }
+
+        /// <summary>
+        /// Gets the total size, in bytes, of all memory allocations that have been made by the 
+        /// application domain with the plugin since it was created, without subtracting memory that
+        /// has been collected (<see cref="AppDomain.MonitoringTotalAllocatedMemorySize"/>).
+        /// </summary>
+        public Int64 TotalAllocatedMemorySize { get; set; }
+
+        /// <summary>
+        /// Gets the total processor time that has been used by all threads while executing in the application
+        /// domain with the plugin, since the process started (<see cref="AppDomain.MonitoringTotalProcessorTime"/>).
+        /// </summary>
+        public TimeSpan TotalProcessorTime { get; set; }
+    }
+}
